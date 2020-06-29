ba30c10b380f2c9f6ced646d2fa54b581f96ba05
GiraffeWeb <46316417+GiraffeWeb@users.noreply.github.com>
2020-06-29 21:33:25 +0700

    Update dynamics.js

--- toucanbot/js/dynamics.js
+++ toucanbot/js/dynamics.js
@@ -231,3 +231,38 @@ function replyMessage(direct) {
     };
     
 };        
+
+
+// Cookie
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+function deleteCookie() {
+    for (i = 0; i < 1000; i++) {
+        tag1 = 'message' + i;
+        tag2 = 'message' + i + 'kind';
+        mess = setCookie(tag1, '0', -1);
+        kind = setCookie(tag2, '0', -1);
+    }
+}
